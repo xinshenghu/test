c31c6d0b9d047864ece831e4b23fcabba769dd22
xinshenghu <235389963@qq.com>
2019-09-26 14:19:10 +0800

    11
    111

---
+++ 0test/create_exchange.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import pika
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='logs', exchange_type='fanout')
+
+connection.close()
