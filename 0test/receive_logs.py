befca3278c58a6962c636c86c32b90dfaa0a3225
xinshenghu <235389963@qq.com>
2019-09-26 14:35:25 +0800

    2
    2

---
+++ 0test/receive_logs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import pika
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='logs', exchange_type='fanout')
+
+result = channel.queue_declare(queue='', exclusive=True)
+queue_name = result.method.queue
+
+channel.queue_bind(exchange='logs', queue=queue_name)
+
+print(' [*] Waiting for logs. To exit press CTRL+C')
+
+
+def callback(ch, method, properties, body):
+    print(" [x] %r" % body)
